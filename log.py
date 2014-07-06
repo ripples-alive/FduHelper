af8818127d5d6fe67a6707179a2a2a6d18b5c4c9
JayvicWen <564311873@qq.com>
2014-07-06 22:24:55 +0800

    Add log

---
+++ log.py
@@ -0,0 +1,25 @@
+import time
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def writeLog(content):
+    log = open('.log', 'a')
+    log.write('=' * 20)
+    log.write(time.strftime('%Y-%m-%d %H:%M:%S'))
+    log.write('=' * 20)
+    log.write('\n')
+
+    if type(content) == type(''):
+        log.write(content + '\n')
+    else:
+        for row in content:
+            for item in row:
+                log.write(item)
+                log.write('\t')
+            log.write('\n')
+
+    log.write('=' * 59)
+    log.write('\n' * 2)
+    log.close()
--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import getpass
 
 from fduConnect import FduConnect
 from fetion import Fetion
+from log import writeLog
 
 connect = FduConnect()
 connect.login('12307130267', getpass.getpass('请输入URP密码：'))
@@ -13,6 +14,7 @@ connect.login('12307130267', getpass.getpass('请输入URP密码：'))
 exer = connect.getExercise()
 for x in exer:
     print x[0] + x[1]
+writeLog(exer)
 
 gpa = connect.getGPA()
 for x in gpa:
@@ -20,22 +22,32 @@ for x in gpa:
     for y in x:
         s += y + '\t'
     print s
+writeLog(gpa)
 
 fet = Fetion('18321985560', getpass.getpass('请输入飞信密码：'))
+fet.sendMsg('此短信仅为验证飞信密码、功能是否正常！')
 
 while True:
     time.sleep(60)
 
     result = connect.getExercise()
-    if result != exer:
+    if len(result) == len(exer) and result != exer:
         print 'New exercise!'
         fet.sendMsg('您的体锻有更新！')
 
         exer = result
+        writeLog(exer)
+    elif len(result) < len(exer):
+        writeLog('There is something wrong with exercise.')
+        writeLog(result)
 
     result = connect.getGPA()
-    if result != gpa:
+    if len(result) > len(gpa):
         print 'New score!'
         fet.sendMsg('您有新的科目出分了！')
 
         gpa = result
+        writeLog(gpa)
+    elif len(result) < len(gpa):
+        writeLog('There is something wrong with the scores.')
+        writeLog(result)
